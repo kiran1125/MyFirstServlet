480a5a038ead3cb2616f2b7351a9dc0c30096c30
Kiran <makamkiran95@gmail.com>
2021-12-19 16:57:49 +0530

    Added : Login Successfull with username Validation

--- src/main/java/com/bridgelabz/LoginServlet.java
+++ src/main/java/com/bridgelabz/LoginServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
@@ -23,10 +25,12 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String user = request.getParameter("user");
+        String namePattern = "^[A-Z]{1}[a-z]{2,}$";
+
         String pwd = request.getParameter("pwd");
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if (userId.equals(user) && password.equals(pwd)) {
+        if (userId.equals(user) && user.matches(namePattern) && password.equals(pwd)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
